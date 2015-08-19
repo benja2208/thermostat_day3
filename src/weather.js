5944b0223487a56b1a48d1d205cdd41d20c54117
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-08-19 16:30:22 +0100

    started multicity

--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,14 @@
 
 
 $(document).ready(function(){
+	var userCity= $("#city").val();
+
+
 	$("#getWeather").click(function(){
-	    jQuery.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q=Montpellier&mode=json&units=metric&cnt=10",function(result){
+	    jQuery.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + "userCity" ,function(result){
         alert("City: "+result.city.name);
         alert("Weather: "+ result.list[0].weather[0].description);
+        alert("Weather: "+ result.list[0].weather[0].temp);
         });
 	  });
 });
