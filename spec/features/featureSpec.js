55444fc2e2487c98e357f15cbcaa8a30e340e5dc
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-08-21 10:02:44 +0100

    added some tests

--- spec/features/featureSpec.js
+++ spec/features/featureSpec.js
@@ -12,6 +12,35 @@ describe("Thermostat", function() {
     it("starts at 20", function() {
       expect("#temperature").toContainText("20");
     });
-  });
+
+    it("increases when click on increase", function() {
+      $("#increase").click();
+      expect("#temperature").toContainText("21");
+    });
+
+    it("decreases when click on descrease", function(){
+      $("#decrease").click();
+      expect("#temperature").toContainText("19");
+    });
+
+    it("resets temperature when click on reset", function(){
+      $("#decrease").click();
+      $("#reset").click();
+      expect("#temperature").toContainText("20");
+    });
+
+    it("can go up and down consistently", function(){
+      $("#decrease").click();
+      expect('#temperature').toContainText('19');
+      $("#increase").click();
+      expect('#temperature').toContainText('20');
+    });
+
+    it("can switch power saving mode off",function(){
+      $("#power_saving_mode").change();
+    });
+
+
+ });
 
 });
